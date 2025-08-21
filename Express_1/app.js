31c4a21c07c84018b0d2ace712ce6f13848ceaef
BrijeshRakhasiya <brijeshrakhasiya587@gmail.com>
2025-08-21 22:57:38 +0530

    Express JS in Hello World

---
+++ Express_1/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/home', (req, res) => {
+  res.sendFile(__dirname+'/public/home.html')
+})
+
+app.get('/about', (req, res) => {
+  res.send('Hello About!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
